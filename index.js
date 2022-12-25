cc885d6edffe37b5fbf149a06c35ac77734a96cf
Allayor <htecgf@gmail.com>
2022-12-25 16:56:36 +0500

    Working state

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const COUNT = 30
-const BOMBS = Math.floor(COUNT * 0.3)
-const CHAR = String.fromCharCode(9671)
+const BOMBS = Math.floor(COUNT * COUNT * 0.10)
+const BOMB_CHAR = String.fromCharCode(9635)
+const CHAR = " "
 const FILL_CHAR = String.fromCharCode(9672)
 
-const arr = Array.from({ length: COUNT }).map((_) => Array.from({ length: 30 }).map((_) => CHAR))
+const arounds = [
+    { x: -1, y: -1 }, { x: 0, y: -1 }, { x: 1, y: -1 },
+    { x: -1, y: 0 }, { x: 1, y: 0 },
+    { x: -1, y: 1 }, { x: 0, y: 1 }, { x: 1, y: 1 }
+]
+
+const arr = Array.from({ length: COUNT }).map((_) => Array.from({ length: COUNT }).map((_) => CHAR))
 
 const getRandomCord = () => {
     return {
@@ -11,11 +18,46 @@ const getRandomCord = () => {
         y: Math.floor(Math.random() * COUNT)
     }
 }
+
+const displayArea = () => {
+    console.log("\n")
+    console.log("     ╔" + "═".repeat(COUNT * 2), "\b╗")
+    arr.forEach((row) => {
+        console.log("     ║", row.join(" ") + "║")
+    })
+    console.log("     ╚" + "═".repeat(COUNT * 2), "\b╝")
+}
+
 const fillBomb = () => {
+    let count = 0;
+    const buff = {}
+    while (count < BOMBS) {
+        let cord = getRandomCord()
+        if (!(`${cord.x}${cord.y}` in buff)) {
+            buff[`${cord.x}${cord.y}`] = count
+            count += 1
+            arr[cord.y][cord.x] = BOMB_CHAR
+        }
+    }
+}
+
+const getCuntBombs = (x, y) => {
+    let count = 0
+    arounds.forEach((cord) => {
+        if (arr[cord.x + x]?.[cord.y + y] === BOMB_CHAR) count += 1
+    })
+    return count
+}
+
+const fillNumbers = () => {
+    for (let i = 0; i < COUNT; i++)
+        for (let j = 0; j < COUNT; j++) {
+            let counts = getCuntBombs(i, j)
+            if (arr[i][j] !== BOMB_CHAR && counts > 0) arr[i][j] = counts
+        }
+    displayArea()
 
 }
 
-
-for (let i = 0; i < COUNT; i++) {
-    console.log(arr[i].join(" "))
-}
+fillBomb()
+fillNumbers()
